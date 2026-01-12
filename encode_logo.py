0e752b49a6287245fde4563b825c03d32eef593d
Emam Hassan <151499841+Emam1523@users.noreply.github.com>
2026-01-12 17:48:38 +0600

    Delete encode_logo.py

--- encode_logo.py
+++
@@ -1,8 +0,0 @@
-import base64
-
-with open(r'e:\Coding\Project\Ramadan-Timing\images\logo.jpg', 'rb') as f:
-    data = base64.b64encode(f.read()).decode('utf-8')
-
-# Write to a text file so I can read it
-with open(r'e:\Coding\Project\Ramadan-Timing\logo_base64.txt', 'w') as f:
-    f.write(data)
